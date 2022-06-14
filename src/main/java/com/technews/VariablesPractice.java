ce7c58099c32b3b7f128ee40e5ca45b35703a0c2
Austin Pratt <hargis32@gmail.com>
2022-06-14 14:04:50 -0500

    practicing Java classes

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,21 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 7;
+
+        double b = 1.84;
+
+        double sum = a + b;
+
+        String c = "poppy";
+
+        String concat = a + c;
+
+        System.out.println(sum);
+
+        System.out.println(concat);
+
+
+    }
+}
